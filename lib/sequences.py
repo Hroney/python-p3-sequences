a10fcbb0c847db3abfe141f8b8aad21acde1a873
Hayden Roney <roneyhayden@gmail.com>
2024-02-07 13:55:19 -0600

    lab done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if (length == 0):
+        print([])
+        return []
+    if (length == 1):
+        print([0])
+        return [0]
+    if (length == 2):
+        print([0,1])
+        return [0,1]
+    arr = [0,1,1]
+    num1 = 1
+    num2 = 1
+    tempnum = 0
+    while (len(arr) < length):
+        tempnum = num1 + num2
+        arr.append(tempnum)
+        num1 = num2
+        num2 = tempnum
+    print(arr)
+    return arr
